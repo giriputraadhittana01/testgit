2af62280f701120c83815241031df7627e4800da
giri putra adhittana <girput01@icloud.com>
2022-03-05 16:12:47 +0700

    Revert "fefe"
    This reverts commit c3b7bbbdf914e9c72e22ac18a556c277d3a9b1f2.

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+<<<<<<< Updated upstream
 	fmt.Println("Boba Enak")
+=======
+>>>>>>> Stashed changes
 	fmt.Println("Giri Putra Adhittana")
 	fmt.Println("Giri")
 }
