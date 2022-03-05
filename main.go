c3b7bbbdf914e9c72e22ac18a556c277d3a9b1f2
giri putra adhittana <girput01@icloud.com>
2022-03-05 16:01:53 +0700

    fefe

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-<<<<<<< Updated upstream
 	fmt.Println("Boba Enak")
-=======
->>>>>>> Stashed changes
 	fmt.Println("Giri Putra Adhittana")
 }
